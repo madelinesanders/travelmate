ce914ef387b610addc9c7c693cab2c9d21b86591
madelinesanders <63517681+madelinesanders@users.noreply.github.com>
2025-03-30 23:25:50 -0400

    Updated gitignore, added login/singup/logout features according to movie tutorial, and created UI base

--- travelmate/settings.py
+++ travelmate/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "trips",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -53,8 +55,7 @@ ROOT_URLCONF = "travelmate.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [os.path.join(BASE_DIR, 'travelmate/templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
