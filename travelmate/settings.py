3685ca0cff86d82e7a77955237f6b06a2f3e2e1c
Srivastava, Ayuri <asrivastava368@gatech.edu>
2025-04-29 03:22:46 -0400

    Heroku functionality

--- travelmate/settings.py
+++ travelmate/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 from decouple import config
 from dotenv import load_dotenv
@@ -87,10 +88,10 @@ WSGI_APPLICATION = "travelmate.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",  # Use SQLite locally if no DATABASE_URL is set
+        conn_max_age=600,
+    )
 }
 
 
